5f2f55160e0725879524173b64d2b479107c595a
Kévin DESCAMPS <kevin.descamps@student.ecp.fr>
2016-10-10 10:35:29 +0200

    Ajout des différents fichiers pour parser l'html de google movie

---
+++ src/com/htmlparser/Seance.java
@@ -0,0 +1,57 @@
+package com.htmlparser;
+import java.util.Calendar;
+
+/**
+ * Class représentant une séance de cinéma 
+ * @author Kévin
+ *
+ */
+public class Seance {
+	
+	private Film film;
+	private Calendar date;
+	private Cinema cinema;
+	
+	public Seance(Film film, int heure, int minutes, Cinema cinema) {
+		this.film = film;
+		this.cinema = cinema;
+		this.date = Calendar.getInstance();
+		this.date.set(Calendar.HOUR_OF_DAY, heure);
+		this.date.set(Calendar.MINUTE, minutes);
+	}
+	
+	public Seance(Film film, Calendar date, Cinema cinema) {
+		super();
+		this.film = film;
+		this.date = date;
+		this.cinema = cinema;
+	}
+
+	public Film getFilm() {
+		return film;
+	}
+
+	public void setFilm(Film film) {
+		this.film = film;
+	}
+
+	public Calendar getDate() {
+		return date;
+	}
+
+	public void setDate(Calendar date) {
+		this.date = date;
+	}
+
+	public Cinema getCinema() {
+		return cinema;
+	}
+
+	public void setCinema(Cinema cinema) {
+		this.cinema = cinema;
+	}
+	
+	
+	
+
+}
