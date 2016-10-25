3f83ded4afcdc0ff765916c0bec7caf079e51fd9
Renaud Dahl <renaud.dahl@student.ecp.fr>
2016-10-25 16:01:42 +0200

    Bugfix

--- src/com/movie/tests/TestCinemaFinder.java
+++ src/com/movie/tests/TestCinemaFinder.java
@@ -57,7 +57,7 @@ public class TestCinemaFinder {
 			cinemaFinder.updateAllSeances();
 			cinemaFinder.printCinemaList();
 
-			cinemaFinder.updateTempsTrajet();
+			cinemaFinder.updateTempsTrajet(null);
 			cinemaFinder.printCinemaList();
 
 
