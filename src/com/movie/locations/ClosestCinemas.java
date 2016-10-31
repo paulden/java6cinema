46461b6ad6bcf7db3e070d5dd7c9dbb442de3681
Renaud Dahl <renaud.dahl@student.ecp.fr>
2016-10-31 12:16:16 +0100

    Refactor ClosestCinemas class

--- src/com/movie/locations/ClosestCinemas.java
+++ src/com/movie/locations/ClosestCinemas.java
@@ -26,20 +26,12 @@ public class ClosestCinemas {
 	 *  Initialize a MyAddress object that will be used to represent the user's position <br>
 	 *  and get a list of cinemas close to him.
 	 */
-	public static MyAddress myAddress = new MyAddress();
-	public static double lat;
-	public static double lng;
-	
-	// Google developer API Key
-	public static String API_key = "AIzaSyAAgLaPXaGmpAC_oaJqjoFZt8A2aQfftTw";
+	private static MyAddress myAddress = new MyAddress();
+
 	private List<Cinema> closestCinemas;
 	
 	// Default constructor
 	public ClosestCinemas(){}
-	
-	public ClosestCinemas(List<Cinema> closestCinemas) {
-		this.closestCinemas = closestCinemas;
-	}
 
 
 	/**
@@ -50,43 +42,44 @@ public class ClosestCinemas {
 	 */
 	public void setClosestCinemas(double radius) throws IOException, JSONException{
 		
-		lat = myAddress.getMyLat();
-		lng = myAddress.getMyLng();
+		double lat = myAddress.getMyLat();
+		double lng = myAddress.getMyLng();
+		String API_key = "AIzaSyAAgLaPXaGmpAC_oaJqjoFZt8A2aQfftTw";
 		
 		// Build the API request with the user's location, the radius and the API Key.
 		HttpClient httpClient = HttpClientBuilder.create().build();
-		String requete = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="
+		String request = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="
 				+ String.valueOf(lat) + "," + String.valueOf(lng) + "&radius=" + String.valueOf(radius)
 				+ "&types=movie_theater&key=" + API_key;		
-		HttpGet getRequest = new HttpGet(requete);		
+		HttpGet getRequest = new HttpGet(request);
 		getRequest.addHeader("accept", "application/json");		
 		HttpResponse res = httpClient.execute(getRequest);
 		
 		if (res.getStatusLine().getStatusCode() != 200) {
-			throw new RuntimeException("Echec: HTTP err code : " + res.getStatusLine().getStatusCode());
+			throw new RuntimeException("Failure: HTTP err code: " + res.getStatusLine().getStatusCode());
 			}
 		
 		// Handling the JSON response
 		String responseString = new BasicResponseHandler().handleResponse(res);		
 		JSONObject obj = new JSONObject(responseString);		
-		JSONArray resultats = obj.getJSONArray("results");
+		JSONArray results = obj.getJSONArray("results");
 		
-		// The Cinema List to receive the rtrieved cinemas
-		List<Cinema> list = new ArrayList<Cinema>();
+		// The Cinema List to receive the retrieved cinemas
+		List<Cinema> list = new ArrayList<>();
 		
 		// Retrieval of all the cinemas found into the JSON response
-		for (int i=0;i<resultats.length();++i) {
+		for (int i=0;i<results.length();++i) {
 							
-			JSONObject obj2 = resultats.getJSONObject(i);
-			String name = resultats.getJSONObject(i).getString("name");
-			String adresse = resultats.getJSONObject(i).getString("vicinity");
+			JSONObject obj2 = results.getJSONObject(i);
+			String name = results.getJSONObject(i).getString("name");
+			String address = results.getJSONObject(i).getString("vicinity");
 			JSONObject geometry = obj2.getJSONObject("geometry");
 			JSONObject location = geometry.getJSONObject("location");
-			double lat = location.getDouble("lat");
-			double lng = location.getDouble("lng");
+			double latitude = location.getDouble("lat");
+			double longitude = location.getDouble("lng");
 			
 			// We create the Cinema objects
-			Cinema newCinema = new Cinema(name, adresse, lat, lng);
+			Cinema newCinema = new Cinema(name, address, latitude, longitude);
 			
 			list.add(newCinema);
 			
